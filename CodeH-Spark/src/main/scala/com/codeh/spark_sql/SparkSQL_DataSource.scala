4e97632b523e7f9f7159b3a6847e73fdd21ec669
jinhua.xu <1569253760@qq.com>
2023-03-29 18:09:53 +0800

    add：新增输出csv文件功能

--- CodeH-Spark/src/main/scala/com/codeh/spark_sql/SparkSQL_DataSource.scala
+++ CodeH-Spark/src/main/scala/com/codeh/spark_sql/SparkSQL_DataSource.scala
@@ -3,6 +3,8 @@ package com.codeh.spark_sql
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
+import java.io.{BufferedWriter, File, FileOutputStream, FileWriter}
+
 /**
  * @className SparkSQL_DataSource
  * @author jinhua.xu
@@ -10,10 +12,12 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
  * @description spark读取各种数据源操作
  * @version 1.0
  */
-object SparkSQL_DataSource {
+object SparkSQL_DataSource extends Serializable {
   def main(args: Array[String]): Unit = {
 
     val conf: SparkConf = new SparkConf().setAppName("SparkSQL_DataSource").setMaster("local[*]")
+//    conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+//    conf.registerKryoClasses(Array(classOf[java.io.BufferedWriter]))
     val spark: SparkSession = SparkSession
       .builder()
       .config(conf)
@@ -25,19 +29,54 @@ object SparkSQL_DataSource {
     // spark读取数据时，忽略丢失的文件
     spark.sql("set spark.sql.files.ignoreMissingFilesDataFrame=true")
 
-    val frame = readCSV(spark)
+    val ds = readCSV(spark).as[Score]
 
 
-    frame.show(false)
+    try {
+      val file = new File("CodeH-Spark/output/score.csv")
 
-    // 写出数据文件
-    //frame.select("age", "name").write.format("parquet").save("D:\\IDEA_Project\\CodeH-BigData\\CodeH-Spark\\input\\people.parquet")
+      val file_parent = new File(file.getParent)
+
+      if (!file_parent.exists()) {
+        file_parent.mkdir()
+        println("父目录创建成功")
+      }
+
+      if (!file.exists()) {
+        file.createNewFile()
+      }
+
+      val bw = new BufferedWriter(new FileWriter(file))
+
+
+      ds.collect().foreach(data => {
+        val sb = new StringBuilder
+        val str = sb.append(data.id).append(",")
+          .append(data.name).append(",")
+          .append(data.class_name).append(",")
+          .append(data.subject).append(",")
+          .append(data.score).append(",")
+          .append(data.teacher).toString()
+        println(str)
+        bw.write(str.toCharArray)
+        bw.newLine()
+        bw.flush()
+      })
+
+      bw.close()
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
+
+
+    //    println(ds.count())
 
     spark.stop()
   }
 
   /**
    * 读取csv文件
+   *
    * @param spark
    * @return
    */
@@ -45,7 +84,7 @@ object SparkSQL_DataSource {
     val frame: DataFrame = spark.read.format("csv")
       .option("sep", ",") // 数据分隔符
       .option("inferSchema", "true")
-      .option("header", "false")  // 是否包含表头
+      .option("header", "true") // 是否包含表头
       .load("CodeH-Spark/input/score.csv")
 
     frame
@@ -56,3 +95,10 @@ object SparkSQL_DataSource {
     frame
   }
 }
+
+case class Score(id: String,
+                 name: String,
+                 class_name: String,
+                 subject: String,
+                 score: String,
+                 teacher: String)
